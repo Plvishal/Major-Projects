ae7b4b34f1de80adab302d81998529fcdbfaf608
Vishal <vishalpal1501@gmail.com>
2023-11-14 22:47:36 +0530

    create Index Route and  also setting the EJS

--- Destination_select/app.js
+++ Destination_select/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const Listing = require('./models/listing.js');
+const path = require('path');
 const app = express();
 
 // DB Connection
@@ -8,6 +9,9 @@ let MONGO_URL = 'mongodb://127.0.0.1:27017/Destination_select';
 async function main() {
   await mongoose.connect(MONGO_URL);
 }
+// Set EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 main()
   .then(() => {
     console.log('Connection Successful!!');
@@ -20,23 +24,29 @@ app.get('/', (req, res) => {
   res.send('Your home roots is working');
 });
 // Create new for testListing
-app.get('/testListing', (req, res) => {
-  let sampleListing = new Listing({
-    title: 'Florican Room',
-    description: 'Private room in farm stay',
-    price: 5000,
-    location: 'Nashik',
-    country: 'India',
-  });
-  // sampleListing
-  //   .save()
-  //   .then((res) => {
-  //     console.log(res);
-  //   })
-  //   .catch((err) => {
-  //     console.log(err);
-  //   });
-  res.send('working');
+// app.get('/testListing', async(req, res) => {
+//   let sampleListing = new Listing({
+//     title: 'Florican Room',
+//     description: 'Private room in farm stay',
+//     price: 5000,
+//     location: 'Nashik',
+//     country: 'India',
+//   });
+//   // await sampleListing
+//   //   .save()
+//   //   .then((res) => {
+//   //     console.log(res);
+//   //   })
+//   //   .catch((err) => {
+//   //     console.log(err);
+//   //   });
+//   res.send('working');
+// });
+
+// Index Route
+app.get('/listing', async (req, res) => {
+  let allListing = await Listing.find();
+  res.render('listing/index.ejs', { allListing });
 });
 app.listen(8080, () => {
   console.log('Server listening on the port :8080');
