51bec7f99fb883d1c88092b343ec9a3b447f94ee
Vishal <vishalpal1501@gmail.com>
2023-12-09 13:02:19 +0530

    when we are params.id to pareant to child  in side the router then on properties are use mergeParams:true

--- Destination_select/routes/reviewRouter.js
+++ Destination_select/routes/reviewRouter.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const reviewRouter = express.Router();
+const reviewRouter = express.Router({ mergeParams: true });
 
 const Review = require('../models/review.js');
 const Listing = require('../models/listing.js');
